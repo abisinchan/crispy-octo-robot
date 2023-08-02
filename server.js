bd988d2c6faa5c5d4e2ed3f5ec04b3702c6e69e9
AE SChan <abigailsinchan@gmail.com>
2023-08-02 02:18:38 -0400

    added delete and comments on server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const uuid = require('./helpers/uuid');
 //port
 const PORT = 3001;
 
-//express server
+//express application
 const app = express();
 
 //middleware
@@ -21,12 +21,13 @@ app.use(express.static('public'));
 
 // GET API ROUTE; request to fetch all notes
 app.get('/api/notes', (req, res) => {
+    //reads db.json
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json('Error reading notes data');
     } else {
-      const notes = JSON.parse(data);
+      const notes = JSON.parse(data);// Parses the JSON data from 'db.json' into an array
       res.status(200).json(notes);
     }
   });
@@ -43,8 +44,11 @@ app.get('*', (req, res) =>
 
 // POST API ROUTE; request to add a note
 app.post('/api/notes', (req, res) => {
+
+//deconstruct
   const { title, text } = req.body;
-  if (title && text) {
+  if (title && text) {  //is both present?
+//create new object
     const newPost = {
       title,
       text,
@@ -57,8 +61,9 @@ app.post('/api/notes', (req, res) => {
         return res.status(500).json('Error reading notes data');
       } else {
         const parsedNotes = JSON.parse(data);
-        parsedNotes.push(newPost);
+        parsedNotes.push(newPost);// Appends the new note object to the array
 
+        // Writes the updated array back to 'db.json'
         fs.writeFile(
           './db/db.json',
           JSON.stringify(parsedNotes, null, 4),
@@ -67,12 +72,14 @@ app.post('/api/notes', (req, res) => {
               console.error(writeErr);
               return res.status(500).json('Error writing notes data');
             } else {
-              console.info('Successfully updated notes!');
+              console.info('Successfully added new note!');
               const response = {
                 status: 'success',
                 body: newPost,
               };
-              console.log(response);
+              console.log(response);// Logs the response object
+
+              //response with the response object as JSON data
               return res.status(201).json(response);
             }
           }
@@ -80,13 +87,38 @@ app.post('/api/notes', (req, res) => {
       }
     });
   } else {
+    //if 'title' or 'text' is missing
     res.status(400).json('Invalid note data');
   }
 });
 
-app.use((req, res) =>
-  res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
-);
+
+// DELETE API ROUTE; request to delete a note
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;//id' parameter received from user click on trash icon
+  
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json('Error reading notes data');
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        // Filters out the note with the specified ID
+        const updatedNotes = parsedNotes.filter((note) => note.id !== noteId);
+  
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 4), (writeErr) => {// Writes the filtered array back to 'db.json'
+          if (writeErr) {
+            console.error(writeErr);
+            return res.status(500).json('Error writing notes data');
+          } else {
+            console.info(`Note with ID ${noteId} deleted successfully!`);
+            return res.status(200).json({ status: 'success', id: noteId });
+          }
+        });
+      }
+    });
+  });
 
 //start server ----------------------------------------------
 app.listen(PORT, () =>
